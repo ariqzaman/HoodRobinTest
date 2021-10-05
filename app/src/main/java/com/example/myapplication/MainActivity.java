469ba997864c786938a4f6c1f7f7391e2c628331
Ariq <azaman3835@bths.edu>
2021-10-05 16:12:44 -0400

    initial

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,85 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextViewResult;
+    private RequestQueue mQueue;
+
+    EditText keyInput;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        //hides header
+        setContentView(R.layout.activity_main);
+        //button and textview to see results
+        mTextViewResult = findViewById(R.id.text_view_result);
+        Button buttonParse = findViewById(R.id.button_parse);
+
+        keyInput = findViewById(R.id.key_input);
+
+        //volley is used to que api activity
+        mQueue = Volley.newRequestQueue(this);
+        //button
+        buttonParse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                jsonParse();
+            }
+        });
+    }
+
+    private void jsonParse() {
+        //removed IBM FOR KEY
+        String key = keyInput.getText().toString();
+
+        String url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol="+key+"&apikey=";
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONObject jsonobject = response.getJSONObject("Time Series (Daily)");
+
+                            JSONObject jsonDate = jsonobject.getJSONObject("2021-10-04");
+
+                            String firstName = jsonDate.getString("1. open");
+
+                            mTextViewResult.append(firstName);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+
+        mQueue.add(request);
+    }
+}
